f3b95c1cfd2f2406eef462b66a27b16d07df46a0
Safiul-Azam <safiulazamriad@gmail.com>
2022-08-26 19:43:36 +0600

    connected mongooes in root file

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvdilna.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const connect = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log('mongodb');
+      } catch (error) {
+       throw error
+      }
+
+}
+mongoose.connection.on('disconnected', ()=> {
+    console.log('mongodb disconnected');
+  });
+mongoose.connection.on('connected', ()=> {
+    console.log('mongodb connected');
+  });
+
+app.get('/', (req, res)=>{
+    res.send('running sultan luxury hotel server')
+})
+app.listen(port,()=>{
+    connect()
+    console.log('listening',port)
+})
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // async function run(){
 //     try{
@@ -51,9 +73,3 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 // run().catch(console.dir)
 
 
-app.get('/', (req, res)=>{
-    res.send('running sultan luxury hotel server')
-})
-app.listen(port,()=>{
-    console.log('listening',port)
-})
